5a20fb251471a3acb967218fe159239d6bdb759b
Tosin <tosiniyiola00@gmail.com>
2022-09-23 12:30:08 +0100

    work

--- login/urls.py
+++ login/urls.py
@@ -2,8 +2,8 @@ from django.urls import include, path
 from .views import home_view, register_view, logout_view
 
 
-urlpatterns = [
-    path("login/", home_view),
-    path("register/", register_view),
-    path("logout/", logout_view),
-]
+# urlpatterns = [
+#     path("login/", home_view),
+#     path("register/", register_view),
+#     path("logout/", logout_view),
+# ]
