7dcd01435ece15bd64c1c64482685aae1af771db
Tosin <tosiniyiola00@gmail.com>
2022-09-23 13:05:29 +0100

    commit

--- notez/settings.py
+++ notez/settings.py
@@ -138,9 +138,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
 
 django_heroku.settings(locals())
