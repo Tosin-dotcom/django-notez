2b474947eae827a9e961de4420553fee4a131324
Tosin <tosiniyiola00@gmail.com>
2022-09-23 11:18:40 +0100

    Added database

--- notez/settings.py
+++ notez/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import dj_database_url
 import django_heroku
 from dotenv import load_dotenv
 from pathlib import Path
@@ -93,6 +94,9 @@ DATABASES = {
     }
 }
 
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
