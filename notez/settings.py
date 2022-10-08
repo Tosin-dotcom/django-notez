bc67b4d69ef16fe2c533930a000c2482b1bf1833
Tosin <tosiniyiola00@gmail.com>
2022-10-08 12:02:17 +0100

    new chnages

--- notez/settings.py
+++ notez/settings.py
@@ -143,8 +143,7 @@ AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
 AWS_DEFAULT_ACL = None
-# 'AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-AWS_S3_CUSTOM_DOMAIN = 'notez-bucket.s3.amazonaws.com'
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 AWS_QUERYSTRING_AUTH = False
 # s3 static settings
@@ -155,6 +154,9 @@ STATICFILES_STORAGE = 'notez.storage_backends.StaticStorage'
 PUBLIC_MEDIA_LOCATION = 'media'
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
 DEFAULT_FILE_STORAGE = 'notez.storage_backends.PublicMediaStorage'
+
+
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 #STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 
 # MEDIA_URL = '/media/'
